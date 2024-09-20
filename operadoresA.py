c85b85bc90ba3facdfabaa1660651b800ca83bfa
Paola-ramirez128 <a.22308051281284@cbtis128.edu.mx>
2024-09-20 09:32:41 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,43 @@
+print("Act 9 clases v3")
+print("Paola Ramirez Mat: 22308051281284")
+# zona de clases 
+class Operadores1284:
+    # zona de funciones
+    def suma1284(self,P,R):
+        s1284= P+R
+        print(f"La suma de {P} + {R} = {s1284}")
+    def rest1284(self,P,R):
+        r1284 = P-R
+        print(f"La resta de {P} - {R} = {r1284}")
+    def mult1284(self,P,R):
+        m1284 = P*R
+        print(f"La multiplicacion de {P} * {R} = {m1284}")
+    def div1284(self,P,R):
+        d1284 = P/R
+        print(f"La division de {P} / {R} = {d1284}")
+    def mod1284(self,P,R):
+        mo1284 = P%R
+        print(f"El modulo de {P} % {R} = {mo1284}")
+    def exp1284(self,P,R):
+        e1284 = P**R
+        print(f"El exponente {P} ** {R} = {e1284}")
+    def co1284(self,P,R):
+        c1284 = P//R
+        print(f"El coeficiente {P} // {R} = {c1284}")
+# zona de creacion del objeto
+operaramirez = Operadores1284()
+# zona de uso de objetos
+print(" Funcion suma")
+operaramirez.suma1284(5,5)
+print(" Funcion resta")
+operaramirez.rest1284(5,5)
+print(" Funcion multiplicacion")
+operaramirez.mult1284(5,5)
+print(" Funcion division")
+operaramirez.div1284(5,5)
+print(" Funcion modulo")
+operaramirez.mod1284(5,5)
+print(" Funcion exponente")
+operaramirez.exp1284(5,5)
+print(" Funcion cociente")
+operaramirez.co1284(5,5)
